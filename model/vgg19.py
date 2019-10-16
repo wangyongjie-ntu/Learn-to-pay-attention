b096229c6f3aea156ed6ebc0d964d8636673eece
wang yongjie <yongjie.wang@ntu.edu.sg>
2019-10-16 09:36:29 +0700

    vgg19.py

--- model/vgg19.py
+++ model/vgg19.py
@@ -5,6 +5,10 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import sys
+sys.path.insert(0, ".")
+from utils.initialize import *
 
 
 model_urls = {
@@ -13,48 +17,84 @@ model_urls = {
 }
 
 class VGG(nn.Module):
-    def __init__(self, num_classes, init_weights = False):
+    def __init__(self, im_size, num_classes, init_weights = "kaimingNormal", attention = False):
         super(VGG, self).__init__()
+        self.im_size = im_size
         self.num_classes = num_classes
         self.init_weights = init_weights
+        self.attention = attention
         self.conv_block1 = self._conv_block(3, 64, 2)
         self.conv_block2 = self._conv_block(64, 128, 3)
         self.conv_block3 = self._conv_block(128, 256, 3)
         self.conv_block4 = self._conv_block(256, 512, 3)
         self.conv_block5 = self._conv_block(512, 512, 3)
         self.conv_block6 = self._conv_block(512, 512, 2, is_pool = True)
+        self.adaptive_conv = nn.Conv2d(512, 512, kernel_size = int(im_size / 32), padding = 0, bias = True)
+        # project to the query dimension
+        self.projector = nn.Conv2d(256, 512, kernel_size = 1, padding = 0, bias = False)
 
+        if self.attention:
+            self.classifier = nn.Linear(512 * 3, self.num_classes, bias = True)
+        else:
+            self.classifier = nn.Linear(512, self.num_classes, bias = True)
+        
+        # weights initialization
+        if init_weights == "kaimingUniform":
+            weights_init_kaimingUniform(self)
+        elif init_weights == "kaimingNormal":
+            weights_init_kaimingNormal(self)
+        elif init_weights == "xavierUniform":
+            weights_init_kaimingNormal(self)
+        elif init_weights == "xavierNormal":
+            weights_init_kaimingNormal(self)
+        else:
+            raise NotImplementedError("Invalid type of initialization")
 
     def forward(self, input_x):
         input_x = self.conv_block1(input_x)
         input_x = self.conv_block2(input_x)
-        input_x = self.conv_block3(input_x)
-        input_x = self.conv_block4(input_x)
-        input_x = self.conv_block5(input_x)
+        l1 = self.conv_block3(input_x)
+        input_x = F.max_pool2d(l1, kernel_size = 2, stride = 2, padding = 0)
+        l2 = self.conv_block4(input_x)
+        input_x = F.max_pool2d(l2, kernel_size = 2, stride = 2, padding = 0)
+        l3 = self.conv_block5(input_x)
+        input_x = F.max_pool2d(l3, kernel_size = 2, stride = 2, padding = 0)
         input_x = self.conv_block6(input_x)
-        return input_x
+        g = self.adaptive_conv(input_x)
 
-    def _initialize_weights(self):
-        NotImplemented
-        return
+        if self.attention:
+            l1 = self.projector(l1)
+            c1, g1 = self.linearAttentionBlock(l1, g)
+            c2, g2 = self.linearAttentionBlock(l2, g)
+            c3, g3 = self.linearAttentionBlock(l3, g)
+            g = torch.cat((g1, g2, g3), dim = 1)
+            x = self.classifier(g)
+        else:
+            c1, c2, c3 = None, None, None
+            x = self.classifier(torch.squeeze(g))
+
+        return [x, c1, c2, c3]
 
     def _conv_block(self, input_depth, num_filters, num_layers, is_pool = False):
         layers = []
         layers.append(nn.Conv2d(input_depth, num_filters, kernel_size = (3, 3), stride = (1, 1), padding = (1, 1)))
         layers.append(nn.BatchNorm2d(num_filters, eps = 1e-05, momentum=0.1, affine=True, track_running_stats=True))
         layers.append(nn.ReLU(inplace = True))
+        if is_pool:
+            layers.append(nn.MaxPool2d(kernel_size = 2, stride = 2, padding = 0, dilation = 1, ceil_mode = False))
 
         for i in range(num_layers - 1):
             layers.append(nn.Conv2d(num_filters, num_filters, kernel_size = (3, 3), stride = (1, 1), padding = (1, 1)))
             layers.append(nn.BatchNorm2d(num_filters, eps = 1e-05, momentum=0.1, affine=True, track_running_stats=True))
             layers.append(nn.ReLU(inplace = True))
-
-        if is_pool:
-            layers.append(nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False))
-        else:
-            layers.append(nn.Conv2d(num_filters, num_filters, kernel_size = (3, 3), stride = (2, 2), padding = (1, 1)))
+            if is_pool:
+                layers.append(nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False))
 
         return nn.Sequential(*layers)
+
+    def _linearAttentionBlock(self, l, g):
+        N, C, W, H = l.size()
+
 
 
 if __name__ == "__main__":
