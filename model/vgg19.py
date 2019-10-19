b4304a83e940d851c38b625ddb7cc39b07131bf4
wang yongjie <yongjie.wang@ntu.edu.sg>
2019-10-19 12:02:38 +0700

    vgg19.py

--- model/vgg19.py
+++ model/vgg19.py
@@ -32,7 +32,6 @@ class VGG(nn.Module):
         self.adaptive_conv = nn.Conv2d(512, 512, kernel_size = int(im_size / 32), padding = 0, bias = True)
         # project to the query dimension
         self.projector = nn.Conv2d(256, 512, kernel_size = 1, padding = 0, bias = False)
-        self.op = nn.Conv2d(512, 1, kernel_size = 1, padding = 0, bias = False) 
 
         if self.attention:
             self.classifier = nn.Linear(512 * 3, self.num_classes, bias = True)
@@ -95,7 +94,8 @@ class VGG(nn.Module):
 
     def linearAttentionBlock(self, l, g, normlize_method = "sigmoid"):
         N, C, W, H = l.size()
-        c = self.op(l + g)
+        op = nn.Conv2d(512, 1, kernel_size = 1, padding = 0, bias = False) 
+        c = op(l + g)
         if normlize_method == "softmax":
             a = F.softmax(c.view(N, -1, 1), dim =  2).view(N, 1, W, H)
         elif normlize_method == "sigmoid":
